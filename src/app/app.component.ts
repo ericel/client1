26a69fc456dd4e1514b8af40b85e8e3efdc27c74
ericele <ericel123new@gmail.com>
2017-02-09 18:13:51 +0900

    mdDailog Added for contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, Optional } from '@angular/core';
+import {MdDialog, MdDialogRef, MdSnackBar} from '@angular/material';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  constructor( private _dialog: MdDialog){
+
+  }
+
+  openDialog() {
+     let dialogRef = this._dialog.open(DialogContact);
+  }
+
+}
+
+@Component({
+  template: `
+    <div class="alert alert-success">contact us:</div>
+    <div>
+    <i class="fa fa-volume-control-phone fa-5x" aria-hidden="true"></i> Tel: 0248 8493 8273
+    </div>
+     <div>
+    <i class="fa fa-envelope" aria-hidden="true"></i> Tel: 0248 8493 8273
+    </div>
+  `,
+})
+export class DialogContact {
+  status: any;
+  constructor(@Optional() public dialogRef: MdDialogRef<DialogContact>) { }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule, MdSnackBarConfig } from '@angular/material';
 
-import { AppComponent } from './app.component';
+import { AppComponent, DialogContact } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogContact
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot()
   ],
   providers: [],
+   entryComponents: [
+    DialogContact
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
